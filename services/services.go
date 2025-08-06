a5559baaf049141cf923312c31303a5a9cb4416e
Birhan habtamu <berahaan@gmail.com>
2025-08-06 13:26:08 +0300

    Initializing the Too do lists

--- services/services.go
+++ services/services.go
@@ -3,6 +3,10 @@ package services
 import "GOLANG/entity"
 
 type VideoService interface {
+	Greet() string
 	SaveVideo(video entity.Video)  // this just saaves the video to local storage woth specified paths
-	FindAllVideos() []entity.Video // this is return slices of videos
+	FindAllVideos() []entity.Video // this is return slices of videos for storage or from databse
+}
+type VideoServices struct {
+	Videos []entity.Video
 }
